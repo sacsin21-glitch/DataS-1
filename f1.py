be05ce037437f4e000d64454b899bfc38b13d339
anants <sacsin21@gmail.com>
2026-07-14 17:11:15 +0530

    Day 2 Experimented with numpy and python Long way to go

---
+++ f1.py
@@ -0,0 +1,5 @@
+print("hello world!")
+#creating a random file. Figuring out how to use python on Jupyter
+#For now I'll be using a notebook
+#I wonder how I am going to run larger codes if any.. we will figure out
+#Peaceout
